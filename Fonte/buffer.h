48b80db8cc3000c9f5b1652bfd94f2a4d8182c36
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2026-07-06 13:04:39 -0300

    reescreve buffer.h com as estruturas do bm

--- Fonte/buffer.h
+++ Fonte/buffer.h
@@ -1,112 +1,77 @@
 #ifndef FBUFFER
-#define FBUFFER 1 // flag controlar os includes
+#define FBUFFER 1
 
-#include <stdint.h> // TRECHO ADICIONADO
+#include <stdint.h>
 
-// CORREÇÃO: Evita redefinir SIZE se macros.h já o definiu
-#ifndef SIZE
-   #define SIZE 4096 // TRECHO ADICIONADO --> Tamanho do bloco;
-#endif
-
-#define MAX_BUFFERS 128 // TRECHO ADICIONADO --> Quantidade de páginas no pool;
-
-#ifndef FMACROS // garante que macros.h não seja reincluída
+#ifndef FMACROS
    #include "macros.h"
 #endif
-//
-#ifndef FTYPES // garante que types.h não seja reincluída
+
+#ifndef FTYPES
   #include "types.h"
 #endif
 
-typedef struct tp_block { // TRECHO ADICIONADO --> Estrutura para armazenar um bloco do buffer;
-    unsigned int nrec; // TRECHO ADICIONADO --> Número de registros armazenados na página;
-    uint32_t position; // TRECHO ADICIONADO --> Número de registro que a página ainda pode receber;
-    char data[4096]; // TRECHO ADICIONADO --> Dados (tuplas);
-} tp_block; // TRECHO ADICIONADO --> Estrutura para armazenar um bloco do buffer;
+// pool so aceita ate uns 10 quadro pq o alocador de memoria do projeto uffsllocType nao deixa alocar mais que 16kb de uma vez e o bufferpool inteiro tem que caber nesse limite
+#define MAX_FRAMES 10
 
-// CORREÇÃO: Mudado para tp_frame para não colidir com o types.h
-typedef struct tp_frame { 
-    unsigned int id; // TRECHO ADICIONADO --> Identificador do buffer;
-    int tabela_id; // TRECHO ADICIONADO --> Identificador da tabela que está armazenada no buffer;
-    unsigned char db; // TRECHO ADICIONADO --> Dirty bit;
-    unsigned char pc; // TRECHO ADICIONADO --> Pin count;
-    tp_block block; // TRECHO ADICIONADO --> Bloco do buffer;
-} tp_frame; 
+// qtd usada quando ninguem escolhe outra na carga do sgbd isso eh o configuravel na carga pedido no trabalho
+#define DEFAULT_FRAME_COUNT 8
 
-typedef struct { // TRECHO ADICIONADO --> Estrutura para armazenar o pool de buffers;
-    tp_frame frames[MAX_BUFFERS]; // TRECHO ADICIONADO --> Array de blocos do buffer;
-} BufferPool; // TRECHO ADICIONADO --> Estrutura para armazenar o pool de buffers;
+// separa o que vai pro disco do que fica so em memoria nada de dirty bit ou pin aqui
+typedef struct DiskPage {
+    unsigned int recordCount;
+    uint32_t usedBytes;
+    char data[SIZE];
+} DiskPage;
 
-// PROTOTIPAÇÃO DAS NOVAS FUNÇÕES:
-void initBufferPool(); // TRECHO ADICIONADO --> Função para inicializar o pool de buffers;
-tp_frame* get_buffer_page(unsigned int block_id, int tabela_id); // TRECHO ADICIONADO --> Função para recuperar uma página do buffer;
-void flushFrame(int frame_id); // TRECHO ADICIONADO --> Função para descarregar o frame especificado no disco;
+typedef struct Frame {
+    int blockId;              // -1 quando o quadro ta vazio
+    int tableId;                // -1 quando vazio, cada tabela tem seu proprio id objeto cod
+    unsigned char dirty;
+    unsigned char pinCount;
+    unsigned char referenced;    // bit de segunda chance do algoritmo do relogio
+    DiskPage page;
+} Frame;
 
-/*
-    Esta função imprime todos os dados carregados numa determinada página do buffer
-    *buffer - Estrutura para armazenar tuplas na memória
-    *s - Estrutura que armazena esquema da tabela para ler os dados do buffer
-    *objeto - Estrutura que armazena dados sobre a tabela que está no buffer
-    *num_page - Número da página a ser impressa
-*/
-int printbufferpoll(tp_buffer *buffpoll, tp_table *s,struct fs_objects objeto, int num_page);
-/*
-    Esta função insere uma tupla em uma página do buffer em que haja espaço suficiente.
-    Retorna ERRO_BUFFER_CHEIO caso não haja espeço para a tupla
+typedef struct {
+    Frame frames[MAX_FRAMES];
+    int activeFrameCount;   // qtd de quadro realmente configurada pode ser menor que MAX_FRAMES
+    int clockHand;            // posicao atual do ponteiro do relogio
+} BufferPool;
 
-    *buffer - Estrutura para armazenar tuplas na meméria
-    *from   - Número da tupla a ser posta no buffer. Este número é relativo a ordem de inserção da
-              tupla na tabela em disco.
-    *campos - Estrutura que armazena esquema da tabela para ler os dados do buffer
-    *objeto - Estrutura que armazena dados sobre a tabela que está no buffer
-*/
-int colocaTuplaBuffer(tp_buffer *buffer, int from, tp_table *campos, struct fs_objects objeto);
+typedef struct {
+    BufferPool *pool;
+    int pageSize;
+    int diskReads;
+    int diskWrites;
+} BufferManager;
 
-/*
-    Esta função recebe um arquivo e o id do buffer,
-    retorna o buffer carregado ou erro. toma toma.
-*/
-tp_buffer *getBlock(unsigned int id, char* filename);
+void initBufferManager(int frameCount);
 
-/*
-    Retorna um buffer iniciado top top. 
-*/
-tp_buffer* initBuffer(unsigned int id); 
+// depois de usar tem que chamar unpinPage se esquecer o pool enche de pino e trava
+Frame *pinPage(unsigned int blockId, int tableId);
 
-/*
-    Esta função recupera uma página do buffer e retorna a mesma em uma estrutura do tipo tupla
-    A estrutura column possui informações de como manipular os dados
-    *campos - Estrutura que armazena esquema da tabela para ler os dados do buffer
-    *objeto - Estrutura que armazena dados sobre a tabela que está no buffer
-    *page - Número da página a ser recuperada (0 a PAGES)
-*/
-PageResult *getPage(tp_table *campos, struct fs_objects objeto, int page); 
+void unpinPage(Frame *frame);
 
-/*
-    Esta função uma determinada tupla do buffer e retorna a mesma em uma estrutura do tipo column;
-    A estrutura column possui informações de como manipular os dados
-    *buffer - Estrutura para armazenar tuplas na meméria
-    *campos - Estrutura que armazena esquema da tabela para ler os dados do buffer
-    *objeto - Estrutura que armazena dados sobre a tabela que está no buffer
-    *page   - Número da página a ser recuperada uma tupla (0 a PAGES)
-    *nTupla - Número da tupla a ser excluida, este número é relativo a página do buffer e não a
-              todos os registros carregados
-*/
-column * excluirTuplaBuffer(tp_buffer *buffer, tp_table *campos, struct fs_objects objeto, int page, int nTupla);
-////
-char *getTupla(tp_table *campos,struct fs_objects objeto, int from);
+void flushBufferPool();
 
-void setTupla(tp_buffer *buffer,char *tupla, int tam, int pos);
-////
-void cria_campo(int , int , char *, int );
+PageResult *getPage(tp_table *campos, struct fs_objects objeto, int page);
 
-/* ----------------------------------------------------------------------------------------------
-    Objetivo:   Utilizada para gravar as mudanças do buffer no disco.
-    Parametros: Buffer (tp_buffer) e dados da tabela (fs_objects)
-    Retorno:    1 para sucesso, 0 para falha.
-   ---------------------------------------------------------------------------------------------*/
-int writeBufferToDisk(tp_buffer *bufferpool, struct fs_objects *objeto);
+// funcao de exibicao usada em misc.c sem relacao com o buffer manager mantida sem mudar nome
+void cria_campo(int, int, char *, int);
 
 void addColumn(column **colList, column *c);
+
+// codigo antigo mantido comentado so de referencia ninguem chama mais depois da migracao pro buffer manager
+/*
+int printbufferpoll(tp_buffer *buffpoll, tp_table *s,struct fs_objects objeto, int num_page);
+int colocaTuplaBuffer(tp_buffer *buffer, int from, tp_table *campos, struct fs_objects objeto);
+tp_buffer *getBlock(unsigned int id, char* filename);
+tp_buffer* initBuffer(unsigned int id);
+column * excluirTuplaBuffer(tp_buffer *buffer, tp_table *campos, struct fs_objects objeto, int page, int nTupla);
+char *getTupla(tp_table *campos,struct fs_objects objeto, int from);
+void setTupla(tp_buffer *buffer,char *tupla, int tam, int pos);
+int writeBufferToDisk(tp_buffer *bufferpool, struct fs_objects *objeto);
+*/
 
 #endif
