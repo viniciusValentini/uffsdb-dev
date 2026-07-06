8eb1e8ecdc2f8964f74cc6736a449e5aea03aaa4
maiqueli-mingoti <maiqueli.eduarda@gmail.com>
2026-07-06 13:06:25 -0300

    inicializa o bm na carga do uffsdb

--- Fonte/uffsdb.c
+++ Fonte/uffsdb.c
@@ -19,6 +19,9 @@
 #ifndef FDATABASE
    #include "database.h"
 #endif
+#ifndef FBUFFER
+   #include "buffer.h"
+#endif
 
 #include "interface/y.tab.h"
 
@@ -27,7 +30,10 @@ db_connected connected;
 
 int main(){
     dbInit(NULL);
-    
+
+    // inicializa o buffer manager com a qtd padrao de quadros pra mudar a quantidade de paginas na carga e so trocar esse numero aqui
+    initBufferManager(DEFAULT_FRAME_COUNT);
+
     printf("uffsdb (16.2).\nType \"help\" for help or \"implement\" for seeing what is or not is implemented in this project.\n\n");
     
     DEBUG_PRINT("UFFS DB Debugging mode.");
